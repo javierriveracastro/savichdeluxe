0816f89e8b12b6501dad5ab36bd1e6d168f2df85
parvusdomus <parvusdomus@gmail.com>
2021-04-25 14:15:34 +0200

    How THOUGH are you?

--- module/actor/actor.js
+++ module/actor/actor.js
@@ -24,14 +24,38 @@ export class SavichDeluxeActor extends Actor {
    */
   _prepareCharacterData(actorData) {
     const data = actorData.data;
-
-    // Make modifications to data here. For example:
-
-    // Loop through ability scores, and add their modifiers to our sheet output.
-    for (let [key, ability] of Object.entries(data.abilities)) {
-      // Calculate the modifier using d20 rules.
-//       ability.mod = Math.floor((ability.value - 10) / 2);
-    }
+    console.log ("ACTORDATA");
+    console.log (actorData);
+    setProperty(this.data, 'data.parada.valor', this.CalcParada(this));
+    setProperty(this.data, 'data.dureza.valor', this.CalcDureza(this));
+    setProperty(this.data, 'data.dureza.armadura', this.CalcArmadura(this));
+        
   }
-
+  
+  CalcParada (actor){
+     var Parada = 0;
+     var PelearValorDado =0;
+     var PelearValorBono =0;
+     const Pelear = actor.items.find((i) => i.type === "habilidad" && i.name === "Pelear");
+     PelearValorDado = getProperty(Pelear, 'data.data.dado') || 0;
+     PelearValorBono = getProperty(Pelear, 'data.data.bono') || 0;
+     Parada = (Math.floor(PelearValorDado/2)) + (Math.floor(PelearValorBono/2)) + 2;
+     return Parada; 
+  }
+  CalcDureza (actor){
+     var Dureza = 0;
+     var ArmaduraVal = 0;
+     var VigorValorDado = actor.data.data.abilities.Vigor.value || 0;
+     var VigorValorBono = actor.data.data.abilities.Vigor.bonus || 0;
+     const Armadura = actor.items.find((i) => i.type === "armadura"  && i.data.data.equipada == "Si");
+     ArmaduraVal = getProperty(Armadura, 'data.data.bono') || 0;
+     Dureza = (Math.floor(VigorValorDado/2)) + (Math.floor(VigorValorBono/2)) + 2 + ArmaduraVal;
+     return Dureza; 
+  }
+  CalcArmadura (actor){
+     var ArmaduraVal = 0;
+     const Armadura = actor.items.find((i) => i.type === "armadura" && i.data.data.equipada == "Si");
+     ArmaduraVal = getProperty(Armadura, 'data.data.bono') || 0;
+     return ArmaduraVal; 
+  }
 }
